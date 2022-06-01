e3f46def158a45439530c38817218be0cf82da71
IsacHolstad <wisac22@gmail.com>
2022-06-01 20:24:57 +0200

    getting data details

--- js/script.js
+++ js/script.js
@@ -5,11 +5,17 @@ const weatherContainer = document.querySelector(".weather-container");
 
 async function weatherData() {
     try{
+        const response = await fetch(myApi);
+        console.log(response);
+        const responseJSON = await response.json();
+        console.log(responseJSON);
+        const weatherDetail = responseJSON.data;
+        console.log(weatherDetail);
 
     }
     catch(error) {
-        weatherContainer.innerHTML += `<h1>404</h1>`;
-        console.log(error)
+        weatherContainer.innerHTML = `<h1>404</h1>`;
+        console.log(error);
     }
 }
 weatherData();
