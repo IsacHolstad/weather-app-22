81e7370bae89e7ca7a164330c4ea2391b02b2afe
IsacHolstad <wisac22@gmail.com>
2022-06-01 20:30:06 +0200

    getting to data

--- js/script.js
+++ js/script.js
@@ -1,16 +1,23 @@
 const myApi = "https://goweather.herokuapp.com/weather/{oslo}";
 console.log(myApi);
+const proxy = "https://noroffcors.herokuapp.com/";
+const corsFixUrl = proxy + myApi;
 
 const weatherContainer = document.querySelector(".weather-container");
 
 async function weatherData() {
     try{
-        const response = await fetch(myApi);
+        const response = await fetch(corsFixUrl);
         console.log(response);
         const responseJSON = await response.json();
         console.log(responseJSON);
         const weatherDetail = responseJSON.data;
         console.log(weatherDetail);
+        for (let i = 0; weatherDetail.length; i++) {
+            console.log(weatherDetail[i].temperature);
+            
+            weatherContainer.innerHTML += `<li>${weatherDetail[i]}</li>`
+        }
 
     }
     catch(error) {
